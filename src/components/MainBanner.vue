58d1a6e7fde486e11e13f84564d44bb182b5d327
Max <maxkss0829@gmail.com>
2022-08-10 14:17:32 +0900

    ios fixed 이슈 해결

--- src/components/MainBanner.vue
+++ src/components/MainBanner.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="w-full h-screen relative">
-    <div class="w-full h-full absolute bg-fixed bg-main-banner bg-center bg-cover"></div>
+    <div class="bg-container w-full h-full absolute bg-main-banner bg-center bg-cover"></div>
     <div class="w-full h-full absolute grid bg-black-alpha place-content-center">
       <div class="text-center text-white">
         <p class="font-black text-6xl mb-5">AI for the future</p>
@@ -19,3 +19,20 @@ export default {
   })
 }
 </script>
+
+<style>
+  /*
+    ios 이슈
+  */
+  @supports ( -webkit-touch-callout : none) {
+    .bg-container {
+    background-attachment:scroll
+    }
+    }
+
+    @supports not ( -webkit-touch-callout : none) {
+    .bg-container {
+    background-attachment: fixed;
+    }
+    }
+</style>
